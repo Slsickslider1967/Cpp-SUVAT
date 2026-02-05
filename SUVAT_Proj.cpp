c15f19c53cda88db82bb8ed721ec7b06720ac0de
Slsickslider <georgiorocco@outlook.com>
2026-02-05 00:55:17 +0000

    Added the rest and tested all, everthing is good

--- SUVAT_Proj.cpp
+++ SUVAT_Proj.cpp
@@ -26,19 +26,20 @@ int main()
     while (run)
     {
         cout << "Welcome to the SUVAT Calc!" << endl;
-        cout << "Please enter the variable you wish to calculate (S, U, V, A, T): ";
+        cout << "Please enter the variable you wish to calculate (S, U, V, A, T): \nOr enter X to quit: \n";
         cin >> Wanted;
+        Wanted = toupper(Wanted);
 
         if (Wanted == 'S')
         {
             cout << "Designate the unknown as X" << endl;
-            cout << "What is the vlaue for U?: ";
+            cout << "What is the value for U?: ";
             cin >> U;
-            cout << "What is the vlaue for V?: ";
+            cout << "What is the value for V?: ";
             cin >> V;
-            cout << "What is the vlaue for A?: ";
+            cout << "What is the value for A?: ";
             cin >> A;
-            cout << "What is the vlaue for T?: ";
+            cout << "What is the value for T?: ";
             cin >> T;
 
             cout << "Calculating S..." << endl;
@@ -49,13 +50,13 @@ int main()
         else if (Wanted == 'U')
         {
             cout << "Designate the unknown as X" << endl;
-            cout << "What is the vlaue for S?: ";
+            cout << "What is the value for S?: ";
             cin >> S;
-            cout << "What is the vlaue for V?: ";
+            cout << "What is the value for V?: ";
             cin >> V;
-            cout << "What is the vlaue for A?: ";
+            cout << "What is the value for A?: ";
             cin >> A;
-            cout << "What is the vlaue for T?: ";
+            cout << "What is the value for T?: ";
             cin >> T;
 
             cout << "Calculating U..." << endl;
@@ -65,15 +66,59 @@ int main()
         }
         else if (Wanted == 'V')
         {
-            // Calculate V
+            cout << "Designate the unknown as X" << endl;
+            cout << "What is the value for S?: ";
+            cin >> S;
+            cout << "What is the value for U?: ";
+            cin >> U;
+            cout << "What is the value for A?: ";
+            cin >> A;
+            cout << "What is the value for T?: ";
+            cin >> T;
+
+            cout << "Calculating V..." << endl;
+
+            double result = calculateV(S, U, A, T);
+            cout << "The calculated value for V is: " << result << endl;
         }
         else if (Wanted == 'A' )
         {
-            // Calculate A
+            cout << "Designate the unknown as X" << endl;
+            cout << "What is the value for S?: ";
+            cin >> S;
+            cout << "What is the value for U?: ";
+            cin >> U;
+            cout << "What is the value for V?: ";
+            cin >> V;
+            cout << "What is the value for T?: ";
+            cin >> T;
+
+            cout << "Calculating A..." << endl;
+
+            double result = calculateA(S, U, V, T);
+            cout << "The calculated value for A is: " << result << endl;
         }
         else if (Wanted == 'T')
         {
-            // Calculate T
+            cout << "Designate the unknown as X" << endl;
+            cout << "What is the value for S?: ";
+            cin >> S;
+            cout << "What is the value for U?: ";
+            cin >> U;
+            cout << "What is the value for V?: ";
+            cin >> V;
+            cout << "What is the value for A?: ";
+            cin >> A;
+
+            cout << "Calculating T..." << endl;
+
+            double result = calculateT(S, U, V, A);
+            cout << "The calculated value for T is: " << result << endl;
+        }
+        else if (Wanted == 'X')
+        {
+            cout << "Exiting the program. Goodbye!" << endl;
+            run = false;
         }
         else
         {
@@ -158,3 +203,108 @@ double calculateU(string s, string v, string a, string t)
         return 0;
     
 }
+
+double calculateV(string s, string u, string a, string t)
+{
+    double S = 0, U = 0, A = 0, T = 0;
+
+    if (s != "x" && s != "X")
+        S = stod(s);
+    
+    if (u != "x" && u != "X")
+        U = stod(u);
+    
+    if (a != "x" && a != "X")
+        A = stod(a);
+    
+    if (t != "x" && t != "X")
+        T = stod(t);
+    
+    //when S is unknown
+    if (U != 0 && A != 0 && T != 0 && s == "x")
+        return U + (A * T);
+    //when U is unknown
+    else if (S != 0 && A != 0 && T != 0 && u == "x")
+        return (S - (0.5 * A * T * T)) / T + (A * T);
+    //when A is unknown
+    else if (S != 0 && U != 0 && T != 0 &&  a == "x")
+        return (2 * S) / T - U + (A * T);  
+    //when T is unknown
+    else if (S != 0 && U != 0 && A != 0 && t == "x")
+        return sqrt(U * U + (2 * A * S));
+    //when all values are provided
+    else if (U != 0 && A != 0 && T != 0)
+        return U + (A * T);
+    else
+        return 0;
+}
+
+double calculateA(string s, string u, string v, string t)
+{
+    double S = 0, U = 0, V = 0, T = 0;
+
+    if (s != "x" && s != "X")
+        S = stod(s);
+    
+    if (u != "x" && u != "X")
+        U = stod(u);
+    
+    if (v != "x" && v != "X")
+        V = stod(v);
+    
+    if (t != "x" && t != "X")
+        T = stod(t);
+    
+    //when S is unknown
+    if (U != 0 && V != 0 && T != 0 && s == "x")
+        return (V - U) / T;
+    //when U is unknown
+    else if (S != 0 && V != 0 && T != 0 && u == "x")
+        return (2 * S) / T - V;  
+    //when V is unknown
+    else if (S != 0 && U != 0 && T != 0 && v == "x")
+        return (2 * S) / T - U;  
+    //when T is unknown
+    else if (S != 0 && U != 0 && V != 0 && t == "x")
+        return sqrt(V * V - U * U) / sqrt(2 * S);
+    //when all values are provided
+    else if (U != 0 && V != 0 && T != 0)
+        return (V - U) / T;
+    else
+        return 0;
+}
+
+double calculateT(string s, string u, string v, string a)
+{
+    double S = 0, U = 0, V = 0, A = 0;
+
+    if (s != "x" && s != "X")
+        S = stod(s);
+    
+    if (u != "x" && u != "X")
+        U = stod(u);
+    
+    if (v != "x" && v != "X")
+        V = stod(v);
+    
+    if (a != "x" && a != "X")
+        A = stod(a);
+    
+    //when S is unknown
+    if (U != 0 && V != 0 && A != 0 && s == "x")
+        return (V - U) / A;
+    //when U is unknown
+    else if (S != 0 && V != 0 && A != 0 && u == "x")
+        return sqrt((2 * S) / A) - (V / A);  
+    //when V is unknown
+    else if (S != 0 && U != 0 && A != 0 && v == "x")
+        return sqrt((2 * S) / A) - (U / A);
+    //when A is unknown 
+    else if (S != 0 && U != 0 && V != 0 && a == "x")
+        return sqrt((2 * S) / ((V * V - U * U) / (2 * S))) - (U / ((V * V - U * U) / (2 * S)));
+    //when all values are provided
+    else if (U != 0 && V != 0 && A != 0)
+        return (V - U) / A;
+    else
+        return 0;
+}   
